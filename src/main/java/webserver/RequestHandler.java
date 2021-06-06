f5a7414eb2ee01ac65b5bc06aa145d4770a9d0fa
hwpark <hwpark11@kakao.com>
2021-06-06 22:56:01 +0900

    feat : 요구사항 3 - POST 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 
+import model.User;
 import model.http.header.*;
 import model.http.request.HttpRequest;
 import org.slf4j.Logger;
@@ -15,10 +16,6 @@ import util.HttpRequestUtils;
 public class RequestHandler extends Thread {
 
   private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
-  private static final String HTTP_REQUEST_LINE_SEPARATOR_REGEX = "\\s";
-  private static final String HTTP_HEADER_ACCEPT_SEPARATOR_REGEX = ";";
-  private static final String HTTP_REQUEST_QUERY_STRING_SEPARATOR = "\\?";
 
   private Socket connection;
 
@@ -36,7 +33,7 @@ public class RequestHandler extends Thread {
         OutputStream out = connection.getOutputStream()) {
       // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-      var httpRequest = readRequest(in);
+      var httpRequest = HttpRequest.builder(in).build();
 
       var dos = new DataOutputStream(out);
 
@@ -48,12 +45,22 @@ public class RequestHandler extends Thread {
       } else if ("/user/form.html".equals(httpRequest.getRequestURI())
           && HttpMethod.GET == httpRequest.getMethod()) {
         body = getResponseResourceData(httpRequest.getRequestURI());
-      } else if ("/user/create".equals(httpRequest.getRequestURI())) {
-
-        // TODO 사용자 추가
-        httpRequest.getRequestParams().forEach((key, value) -> log.debug("{} : {}", key, value));
-
+      } else if ("/user/create".equals(httpRequest.getRequestURI())
+          && HttpMethod.POST == httpRequest.getMethod()) {
         body = new byte[0];
+
+        Map<String, String> requestParam =
+            HttpRequestUtils.parseQueryString(httpRequest.getContents());
+
+        User user =
+            new User(
+                requestParam.get("userId"),
+                requestParam.get("password"),
+                requestParam.get("name"),
+                requestParam.get("email"));
+
+        log.debug("user : {}", user);
+
       } else {
         body = "Hello World".getBytes();
       }
@@ -87,80 +94,5 @@ public class RequestHandler extends Thread {
     } catch (IOException e) {
       log.error(e.getMessage());
     }
-  }
-
-  private HttpRequest readRequest(InputStream in) throws IOException {
-
-    var requestBuilder = HttpRequest.builder();
-
-    var headers = new HttpHeaders();
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
-    String data;
-
-    while (!checkRequestEnd(data = br.readLine())) {
-
-      log.debug("request : {}", data);
-      String[] tokens = data.split(HTTP_REQUEST_LINE_SEPARATOR_REGEX);
-
-      // request line
-      if (tokens.length == 3) {
-
-        String methodStr = tokens[0];
-        String uriStr = tokens[1];
-        String versionStr = tokens[2];
-
-        String[] uriTokens = uriStr.split(HTTP_REQUEST_QUERY_STRING_SEPARATOR);
-
-        requestBuilder
-            .method(HttpMethod.parse(methodStr))
-            .requestURI(uriTokens[0])
-            .version(HttpVersion.parse(versionStr));
-
-        if (uriTokens.length > 1) {
-          Map<String, String> requestParamsMap = HttpRequestUtils.parseQueryString(uriTokens[1]);
-
-          requestParamsMap.forEach(requestBuilder::addRequestParam);
-        }
-
-      } else {
-        var headerPair = HttpRequestUtils.parseHeader(data);
-
-        if (headerPair == null) {
-          continue;
-        }
-
-        var header = HttpHeader.parse(headerPair.getKey());
-
-        switch (header) {
-          case HOST:
-            requestBuilder.requestHost(headerPair.getValue());
-            break;
-
-          case CONNECTION:
-            requestBuilder.connection(HttpConnection.parse(headerPair.getValue()));
-            break;
-          case ACCEPT:
-            String[] acceptTokens = headerPair.getValue().split(HTTP_REQUEST_LINE_SEPARATOR_REGEX);
-
-            for (String acceptToken : acceptTokens) {
-              headers.addAccept(MediaType.parse(acceptToken));
-            }
-
-            break;
-          default:
-            break;
-        }
-      }
-    }
-
-    requestBuilder.headers(headers);
-
-    return requestBuilder.build();
-  }
-
-  private boolean checkRequestEnd(String data) {
-    return data == null || "".equals(data) || "\r\n".equals(data);
   }
 }
