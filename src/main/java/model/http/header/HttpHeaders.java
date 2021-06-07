6039a2b976ea9674c091aeb717747c0ae8722c45
hwpark <hwpark11@kakao.com>
2021-06-07 22:47:45 +0900

    feat : cookie 처리 추가

--- src/main/java/model/http/header/HttpHeaders.java
+++ src/main/java/model/http/header/HttpHeaders.java
@@ -1,10 +1,17 @@
 package model.http.header;
 
+import model.http.request.Cookie;
+import util.HttpRequestUtils;
+
 import java.util.*;
+
+import static util.CommonUtils.equalsIgnoreCase;
 
 public class HttpHeaders {
 
   private static final String HTTP_HEADER_SEPARATOR = ";";
+
+  private static final String HTTP_COOKIES_KEY = "Cookie";
 
   private final Map<String, String> headers = new HashMap<>();
 
@@ -44,6 +51,27 @@ public class HttpHeaders {
     }
 
     return Long.parseLong(contentLength);
+  }
+
+  public List<Cookie> getCookies() {
+
+    List<Cookie> cookies = new ArrayList<>();
+
+    String cookieStr = headers.get(HTTP_COOKIES_KEY);
+
+    Map<String, String> cookieMap = HttpRequestUtils.parseCookies(cookieStr);
+
+    cookieMap.forEach((key, value) -> cookies.add(new Cookie(key, value)));
+
+    return cookies;
+  }
+
+  public Cookie getCookie(String name) {
+
+    return getCookies().stream()
+        .filter(cookie -> equalsIgnoreCase(cookie.getKey(), name))
+        .findAny()
+        .orElse(null);
   }
 
   public Map<String, String> getHeaders() {
