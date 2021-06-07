7f61084a31203f64dc79937f4b2dd648b2e4fed1
hwpark <hwpark11@kakao.com>
2021-06-07 22:23:16 +0900

    feat : equalsIgnoreCase 추가

--- src/main/java/util/CommonUtils.java
+++ src/main/java/util/CommonUtils.java
@@ -15,4 +15,17 @@ public class CommonUtils {
   public static boolean isNotBlank(String str) {
     return !isBlank(str);
   }
+
+  public static boolean equalsIgnoreCase(String s1, String s2) {
+
+    if (s1 == null) {
+      return false;
+    }
+
+    if (s2 == null) {
+      return false;
+    }
+
+    return s1.equalsIgnoreCase(s2);
+  }
 }
