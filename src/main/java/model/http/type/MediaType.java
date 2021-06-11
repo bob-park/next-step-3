3d48b112e87674fc258bd7e55af8a54afb6ad019
phw11 <hwpark11@kakao.com>
2021-06-12 00:22:13 +0900

    feat : media type 추가

--- src/main/java/model/http/type/MediaType.java
+++ src/main/java/model/http/type/MediaType.java
@@ -8,6 +8,7 @@ public enum MediaType {
   TEXT_CSS("text/css"),
   TEXT_JS("text/js"),
   APPLICATION_JSON("application/json"),
+  APPLICATION_X_WWW_FORM_URLENCODED("application/x-www-form-urlencoded"),
   ;
 
   private final String value;
